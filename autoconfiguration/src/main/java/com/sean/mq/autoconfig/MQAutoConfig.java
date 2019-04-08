0fd82936c3d0b6d71402aad363824748961b7960
Sean <shengsheng.ss@alibaba-inc.com>
2019-04-08 14:27:28 +0800

    develop commit

---
+++ autoconfiguration/src/main/java/com/sean/mq/autoconfig/MQAutoConfig.java
@@ -0,0 +1,19 @@
+/**
+ * io.terminus
+ * Copyright(c) 2012-2018 All Rights Reserved.
+ */
+package com.sean.mq.autoconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author sean
+ * @version Id:,v0.1 2018/6/12 上午10:15 sean Exp $
+ * @description
+ */
+@Configuration
+@ComponentScan("io.terminus.mq")
+public class MQAutoConfig {
+
+}
